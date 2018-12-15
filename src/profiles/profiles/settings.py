3c93c2ff56f1e9b4a0365957f4a4568d46e3ff76
Monkey-D-Code <theartofdeception09@gmail.com>
2018-12-15 16:15:51 +0530

    Added Hashing Algorithms & files directories

--- src/profiles/profiles/settings.py
+++ src/profiles/profiles/settings.py
@@ -14,8 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
+TEMPLATE_DIR = os.path.join(BASE_DIR , 'templates')
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+MEDIA_DIR = os.path.join(BASE_DIR , 'media')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'profiles.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -85,7 +86,17 @@ DATABASES = {
 
 
 # Password validation
-# https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
+
+
+
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -93,6 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS' : {
+            'min_length' : 8,
+        }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -121,4 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-AUTH_USER_MODEL = 'profiles_api.UserProfile'
+# AUTH_USER_MODEL = 'profiles_api.UserProfile'
+STATICFILES_DIRS = [STATIC_DIR,]
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
--- src/profiles/profiles_api/models.py
+++ src/profiles/profiles_api/models.py
@@ -1,44 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUserManager
+from django.contrib.auth.models import User
 
 # Create your models here.
-class UserProfile(AbstractBaseUser , PermissionsMixin):
-    """ REPRESENTS A USER PROFILE IS THE MODEL """
+class Customer(models.Model):
 
-        email = models.EmailField(max_length=255,unique=True)
-        name = models.CharField(max_length=255)
-        is_active = models.BooleanField(default=True)
-        is_staff = models.BooleanField(default=False)
+    user = models.OneToOneField(User ,on_delete=models.CASCADE)
 
-        objects = UserProfileManager()
+    phone_number = models.IntegerField(max_length=255 , unique=True)
+    delivery_address = models.CharField(max_length=255 , unique=True)
+    profile_pic = models.ImageField(upload_to='customer/profile_pics' , blank=True)
 
-        USERNAME_FIELD = 'email'
-        REQUIRED_FIELDS = ['name']
-
-        def get_full_name(self):
-            return self.name
-
-        def get_short_name(self):
-            return self.name
-
-        def __str__(self):
-            return self.email
-
-class UserProfileManager(BaseUserManager):
-
-    def create_user(self,email,name,password):
-        if not email:
-            raise ValueError("Users must have an email")
-
-        email = self.email.normalize_email(email)
-        user = self.model(email=email,name=name)
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self,email,name,password):
-        user = self.create_user(email,name,password)
-        user.is_superuser = True
-        user.is_staff = True
-        user.save(using=self._db)
+    def __str__(self):
+        return self.user.username
